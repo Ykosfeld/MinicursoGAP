947f10f6d448674fca01dfdafc462e357e3d9940
Ykosfeld <yuri.kosfeld@gmail.com>
2024-10-09 22:05:10 -0300

    Feat: faster max_nr_steps

--- collatz/collatz.py
+++ collatz/collatz.py
@@ -24,4 +24,33 @@ def max_nr_steps(n_max: int) -> list[int, int]:
 
     return [max_n, max_steps]
 
-print(max_nr_steps(1000))
+def max_nr_steps_fast(n_max: int) -> list[int, int]:
+    record = dict()
+    
+    max_n = 0
+    max_steps = 0
+
+    for n in range(1, n_max + 1):
+        n_steps = 0
+        n_copy = n
+        
+        while True:
+            if n_copy == 1:
+                break
+            
+            colltaz_n = collatz(n_copy)
+            n_steps += 1
+
+            if colltaz_n in record.keys():
+                n_steps += record[colltaz_n]
+                break
+            
+            n_copy = colltaz_n
+        
+        record[n] = n_steps
+        
+        if n_steps > max_steps:
+            max_steps = n_steps
+            max_n = n
+
+    return [max_n, max_steps]
